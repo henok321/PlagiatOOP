3010cd463a290f302a794cb80f45aeae0aaff34f
Hendrik Brinkmann <hendrik@brinkmann-dev.eu>
2019-04-01 03:00:30 +0200

    ignore ecplise in gitignore

--- src/main/java/plagiarism/Main.java
+++ src/main/java/plagiarism/Main.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 
 /**
+ * v
  * Initialize an instance of  {@link Plagiarism}, get user input and execute the program.
  *
  * @author Hendrik Brinkmann
@@ -17,9 +18,9 @@ public class Main {
         String path = null;
 
         try {
-        	path = parseCliParams(args);
-        } catch(InvalidCommandLineParameter e) {
-        	System.err.println("Invalid parameter.");
+            path = parseCliParams(args);
+        } catch (InvalidCommandLineParameter e) {
+            System.err.println("Invalid parameter.");
         }
 
         File file = new File(path);
@@ -34,11 +35,11 @@ public class Main {
         System.out.println(plagiarism);
         System.out.println("Checksum:\t" + plagiarism.checksum());
     }
-    
+
     private static String parseCliParams(String[] args) throws InvalidCommandLineParameter {
-        if(args==null)
+        if (args == null)
             throw new InvalidCommandLineParameter("missing input file paramter");
-        else if((args[0].length() < 3) || !(args[0].startsWith("-i=")))
+        else if ((args[0].length() < 3) || !(args[0].startsWith("-i=")))
             throw new InvalidCommandLineParameter("invalid input file parameter");
         else
             return args[0].split("=")[1];
