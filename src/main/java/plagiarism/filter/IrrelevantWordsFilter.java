d91b8dbe473193586d6aa332d267221909337dae
Hendrik Brinkmann <hendrik@brinkmann.dev>
2019-12-16 20:43:37 +0100

    java 9 factory method for sets

--- src/main/java/plagiarism/filter/IrrelevantWordsFilter.java
+++ src/main/java/plagiarism/filter/IrrelevantWordsFilter.java
@@ -3,8 +3,6 @@ package plagiarism.filter;
 import java.io.FilterReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -14,18 +12,16 @@ import java.util.Set;
  */
 public class IrrelevantWordsFilter extends FilterReader {
 
-    private static Set<String> irrelevantWords = new HashSet<String>(Arrays.asList(
-            new String[]{
-                    "die", "der", "und", "in", "zu", "den", "das", "von", "sie", "ist", "des", "sich", "mit", "dem", "dass",
-                    "er", "es", "ein", "ich", "auf", "so", "eine", "auch", "als", "an", "nach", "wie", "im", "man", "aber",
-                    "aus", "durch", "wenn", "nur", "war", "noch", "werden", "bei", "hat", "wir", "was", "wird", "sein",
-                    "einen", "welche", "sind", "oder", "zur", "um", "haben", "einer", "mir", "ihm", "einem", "ihr", "uns",
-                    "da", "zum", "kann", "doch", "vor", "mich", "ihn", "du", "hatte", "seine", "am", "denn", "nun",
-                    "unter", "sehr", "selbst", "schon", "hier", "bis", "habe", "ihre", "dann", "ihnen", "seiner", "alle",
-                    "meine", "vom", "wo", "eines", "sei", "ja", "wurde", "seinen", "wohl", "dieses", "ihren", "würde",
-                    "diesen", "sondern", "weil", "welcher", "diesem", "alles", "waren", "will", "mein", "also", "soll",
-                    "worden", "lassen", "dies", "machen", "ihrer"}
-    ));
+    private static final Set<String> irrelevantWords = Set.of(
+            "die", "der", "und", "in", "zu", "den", "das", "von", "sie", "ist", "des", "sich", "mit", "dem", "dass",
+            "er", "es", "ein", "ich", "auf", "so", "eine", "auch", "als", "an", "nach", "wie", "im", "man", "aber",
+            "aus", "durch", "wenn", "nur", "war", "noch", "werden", "bei", "hat", "wir", "was", "wird", "sein",
+            "einen", "welche", "sind", "oder", "zur", "um", "haben", "einer", "mir", "ihm", "einem", "ihr", "uns",
+            "da", "zum", "kann", "doch", "vor", "mich", "ihn", "du", "hatte", "seine", "am", "denn", "nun",
+            "unter", "sehr", "selbst", "schon", "hier", "bis", "habe", "ihre", "dann", "ihnen", "seiner", "alle",
+            "meine", "vom", "wo", "eines", "sei", "ja", "wurde", "seinen", "wohl", "dieses", "ihren", "würde",
+            "diesen", "sondern", "weil", "welcher", "diesem", "alles", "waren", "will", "mein", "also", "soll",
+            "worden", "lassen", "dies", "machen", "ihrer");
 
     public IrrelevantWordsFilter(Reader in) {
         super(in);
