55fc7635055ffa9c55fe04221805e47de400441e
Hendrik Brinkmann <hendrik@brinkmann.dev>
2021-05-17 01:18:03 +0200

    test: Parameterized tests

--- src/test/java/plagiarism/filter/IrrelevantWordsFilterTest.java
+++ src/test/java/plagiarism/filter/IrrelevantWordsFilterTest.java
@@ -3,37 +3,26 @@ package plagiarism.filter;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.StringReader;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class IrrelevantWordsFilterTest {
 
   private IrrelevantWordsFilter irrelevantWordsFilter;
 
-  @Test
-  void shouldFilterIrrelevantWord() throws Exception {
-    irrelevantWordsFilter = new IrrelevantWordsFilter(new StringReader("die"));
-    final int expected = 0;
-    final char[] cbuf = "die".toCharArray();
-
-    final int actual = irrelevantWordsFilter.read(cbuf);
-    assertThat(actual).isEqualTo(expected);
+  static Stream<Arguments> filterParameters() {
+    return Stream.of(Arguments.of("die", 0), Arguments.of("pizza", 5), Arguments.of("", -1));
   }
 
-  @Test
-  void shouldNotFilterRelevantWord() throws Exception {
-    irrelevantWordsFilter = new IrrelevantWordsFilter(new StringReader("pizza"));
-    final int expected = 5;
-    final char[] cbuf = "pizza".toCharArray();
+  @ParameterizedTest
+  @MethodSource("filterParameters")
+  void filterTest(final String input, final int expected) throws Exception {
+    irrelevantWordsFilter = new IrrelevantWordsFilter(new StringReader(input));
+    final char[] cbuf = new char[128];
 
     final int actual = irrelevantWordsFilter.read(cbuf);
-    assertThat(actual).isEqualTo(expected);
-  }
-
-  @Test
-  void shouldEndStream() throws Exception {
-    irrelevantWordsFilter = new IrrelevantWordsFilter(new StringReader(""));
-    final int expected = -1;
-    final int actual = irrelevantWordsFilter.read();
     assertThat(actual).isEqualTo(expected);
   }
 }
